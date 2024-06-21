76b63cfb75df4a0a1617c488eadc77686223b2fa
Jeff Doar <jdoar@swbell.net>
2024-06-20 20:13:03 -0500

    Initial Commit

---
+++ FontLeaflet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FontLeaflet
+//
+//  Created by Jeff Doar on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FontLeaflet/FontLeafletApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontLeafletApp.swift
+//  FontLeaflet
+//
+//  Created by Jeff Doar on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FontLeafletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
